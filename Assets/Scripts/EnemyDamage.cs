bc42c9728cebf642ba7ba8acf6f802fe3e367da2
marquemorquemer <m.solssilva@uandresbello.edu>
2021-04-06 21:31:38 -0400

    Agregué enemigo movimiento, enemigo daño, enemigos, colliders

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public float damage;
+    public float damageTime;
+    private float counter;
+    public bool hasHit;
+    bool playerInRange;
+    public Health hp;
+    public GameObject player;
+    
+
+
+    void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasHit)
+        {
+            counter += Time.deltaTime;
+            if (counter > damageTime)
+            {
+                counter = 0;
+                hasHit = false;
+            }
+
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            if (!hasHit)
+            {
+                Destroy(gameObject);
+                hasHit = true;
+                SceneManager.LoadScene("Inicial");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public int hp = 5;
+    public bool enemy;
+
+
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
